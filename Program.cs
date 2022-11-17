f0fda4d352976416253d409107f8b9a7e202d581
kapiushon <amiller.swe@pm.me>
2022-11-16 22:12:09 -0500

    working example

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var rnd = new Random();
+var all_characters = File.ReadAllLines("character_set.txt").SelectMany(l => l.Split(" "));
+
+Func<IEnumerable<string>, int, IEnumerable<string>> RowValues = (choice, count) => choice.OrderBy(_ => rnd.Next()).Take(count);
+Func<string, string> ToDiv = (input) => $"<div>{input}</div>";
+Func<IEnumerable<string>, string> ToRow = (input) => string.Join(Environment.NewLine, input.Select(ToDiv));
+Func<string> GenerateRow = () => ToRow(RowValues(all_characters, 6));
+
+
+var output = File.ReadAllLines("index.html").Select(l => l.Replace("{{ROW}}", GenerateRow()));
+File.WriteAllLines("output.html", output);
